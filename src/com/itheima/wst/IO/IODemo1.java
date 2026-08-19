887c8676d72c0e6a07b5d73a1d71de7714efb3e8
W sir <15053305396@163.com>
2026-08-19 11:54:29 +0800

    26.8.19 IO流

---
+++ src/com/itheima/wst/IO/IODemo1.java
@@ -0,0 +1,30 @@
+package com.itheima.wst.IO;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class IODemo1 {
+    public static void main(String[] args) throws IOException {
+
+        //创建流对象，需要异常处理，参数为处理文件的路径
+        FileOutputStream fos1=new FileOutputStream("D:\\work space\\IODemo\\src\\com\\itheima\\wst\\IO\\fileDemo\\Demo1.txt");
+        fos1.write(12);
+        fos1.close();             //释放资源
+
+        //如果文件不存在
+        FileOutputStream fos2=new FileOutputStream("D:\\work space\\IODemo\\src\\com\\itheima\\wst\\IO\\fileDemo\\Demo2.txt");
+        fos2.write(123);
+        fos2.close();             //释放资源
+
+        //如果父级不存在，报错
+        /*FileOutputStream fos3=new FileOutputStream("D:\\IODemo\\src\\com\\itheima\\wst\\IO\\fileDemo\\Demo2.txt");
+        fos1.write(123);
+        fos1.close();             //释放资源*/
+
+        //如果存在，会清空
+        FileOutputStream fos4=new FileOutputStream("D:\\work space\\IODemo\\src\\com\\itheima\\wst\\IO\\fileDemo\\Demo3.txt");
+        fos4.write(123);
+        fos4.close();             //释放资源
+    }
+}
