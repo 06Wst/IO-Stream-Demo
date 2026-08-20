3bb513a3c33e8031b6816d7f9e9f96a779cba0e0
W sir <15053305396@163.com>
2026-08-20 12:44:37 +0800

    26.8.10 IO流 FileInputStream 的拷贝文件

--- src/com/itheima/wst/IO/IODemo6.java
+++ src/com/itheima/wst/IO/IODemo6.java
@@ -6,13 +6,32 @@ import java.io.IOException;
 
 public class IODemo6 {
     public static void main(String[] args) throws IOException {
-        FileInputStream fis1=new FileInputStream("D:\\work space\\IODemo\\src\\com\\itheima\\wst\\IO\\fileDemo\\Demo5.txt");
+        /*FileInputStream fis1=new FileInputStream("D:\\work space\\IODemo\\src\\com\\itheima\\wst\\IO\\fileDemo\\Demo5.txt");
         FileOutputStream fos1=new FileOutputStream("D:\\work space\\IODemo\\src\\com\\itheima\\wst\\IO\\fileDemo\\Demo1.txt");
         int b;
         while ((b=fis1.read())!=-1){
             fos1.write(b);
         }
         fos1.close();
-        fis1.close();
+        fis1.close();*/
+
+        FileInputStream fis2=new FileInputStream("D:\\work space\\IODemo\\src\\com\\itheima\\wst\\IO\\fileDemo\\Demo5.txt");
+        FileOutputStream fos2=new FileOutputStream("D:\\work space\\IODemo\\src\\com\\itheima\\wst\\IO\\fileDemo\\Demo1.txt");
+        int len;
+        byte[] b=new byte[2];
+        while ((len=fis2.read(b))!=-1){             //一次读取2字节
+            System.out.print(len+" ");            //返回值为实际读取的字节数量
+            String str=new String(b,0,len);             //将字节数组转换为字符串
+            System.out.println(str);
+            fos2.write(str.getBytes());
+        }
+        /*
+        String str=new String(b);
+        最后会输出空格是为什么：
+        因为最后只读取到d，没有覆盖空格，所以空格保留
+         */
+        fos2.close();
+        fis2.close();
+
     }
 }
