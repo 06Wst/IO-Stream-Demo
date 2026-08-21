b004ded0b25ada51216d00164b437ccb0b9d0e59
W sir <15053305396@163.com>
2026-08-21 14:49:29 +0800

    26.8.21 IO流序列化流的使用

---
+++ src/com/itheima/wst/IO/IODemo17_ObjectOutputStreamDemo1.java
@@ -0,0 +1,16 @@
+package com.itheima.wst.IO;
+
+import com.itheima.wst.IO.entity.Student;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class IODemo17_ObjectOutputStreamDemo1 {
+    public static void main(String[] args) throws IOException {
+        Student s1=new Student("张三",18);           //该类必须实现Serializable接口
+        ObjectOutputStream oos1=new ObjectOutputStream(new FileOutputStream("D:\\work space\\IODemo\\src\\com\\itheima\\wst\\IO\\fileDemo\\Demo9.txt"));
+        oos1.writeObject(s1);
+        oos1.close();
+    }
+}
---
+++ src/com/itheima/wst/IO/entity/Student.java
@@ -0,0 +1,29 @@
+package com.itheima.wst.IO.entity;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
